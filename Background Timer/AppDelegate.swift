c58a716ec2edd1a9ce59a5462d28a03ff4b711a8
Mark Filter <markfilter@outlook.com>
2018-02-28 13:46:21 -0500

    Adds gif for Readme

--- Background Timer/AppDelegate.swift
+++ Background Timer/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var bgTask: UIBackgroundTaskIdentifier = UIBackgroundTaskInvalid
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -20,25 +21,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        var count = 0
+        
+        let _: Timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer:Timer) in
+            count += 1
+            print("Timer Count: ", count)
+            
+            if count >= 10 {
+                timer.invalidate()
+                self.endBackground()
+            }
+        }
+        
+        bgTask = application.beginBackgroundTask(expirationHandler: {
+            // this is the code that fires when iOS ends your background task.
+            self.endBackground()
+        })
+    }
+    
+    func endBackground() {
+        UIApplication.shared.endBackgroundTask(bgTask)
+        bgTask = UIBackgroundTaskInvalid
+        print("endBackgroud Callback Method Fired!")
     }
 
+    
+    
+    
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
 
 
