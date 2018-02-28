d3553a38847547e22ef7b606fd9fcd3edd5f9426
Mark Filter <markfilter@outlook.com>
2018-02-28 13:15:24 -0500

    Initial Commit

---
+++ Background Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Background Timer
+//
+//  Created by Mark Filter on 2/28/18.
+//  Copyright © 2018 Mark Filter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
